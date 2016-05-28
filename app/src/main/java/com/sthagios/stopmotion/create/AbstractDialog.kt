96f7cc8962582fc6fd0b361edb2d85d5592433e9
stephan <stephan.hagios@gmail.com>
2016-05-28 16:10:36 +0200

    Added dialogs for burst attributes

---
+++ app/src/main/java/com/sthagios/stopmotion/create/AbstractDialog.kt
@@ -0,0 +1,124 @@
+package com.sthagios.stopmotion.create
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.Context
+import android.os.Bundle
+import com.afollestad.materialdialogs.MaterialDialog
+import com.sthagios.stopmotion.R
+
+/**
+ * Stopmotion
+ *
+ * @author  stephan
+ * @since   28.05.16
+ */
+open class AbstractDialog() : DialogFragment() {
+
+    interface Callback {
+        fun timeChosen(time: Int)
+
+        fun amountChosen(amount: Int)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is Callback) {
+            mListener = context
+        } else
+            throw Exception("${context.toString()} must implement Callback")
+    }
+
+    lateinit var mListener: Callback
+
+    var mValue = 0
+
+    val BUNDLE_VALUE = "BUNDLE_VALUE"
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState!!.putInt(BUNDLE_VALUE, mValue)
+        super.onSaveInstanceState(outState)
+    }
+}
+
+class BurstTimeDialog : AbstractDialog() {
+    companion object {
+        val BUNDLE_VALUE = "BUNDLE_VALUE"
+
+        fun newInstance(value: Int): BurstTimeDialog {
+            val fragment = BurstTimeDialog()
+            val args = Bundle();
+            args.putInt(BUNDLE_VALUE, value);
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        if (arguments != null) {
+            mValue = arguments.getInt(BUNDLE_VALUE, 0);
+        }
+
+        if (savedInstanceState != null) {
+            mValue = savedInstanceState.getInt(BUNDLE_VALUE, 0);
+        }
+
+        val dialog = MaterialDialog.Builder(activity)
+                .title("Choose Burst Interval")
+                .items(R.array.burst_times)
+                .itemsCallbackSingleChoice(mValue, { materialDialog, view, i, charSequence ->
+                    mValue = i
+                    true
+
+                })
+                .alwaysCallSingleChoiceCallback()
+                .positiveText("ok")
+                .onPositive({ materialDialog, dialogAction -> mListener.timeChosen(mValue) })
+                .show();
+
+        return dialog
+    }
+}
+
+class BurstAmountDialog : AbstractDialog() {
+    companion object {
+        val BUNDLE_VALUE = "BUNDLE_VALUE"
+
+        fun newInstance(value: Int): BurstAmountDialog {
+            val fragment = BurstAmountDialog()
+            val args = Bundle();
+            args.putInt(BUNDLE_VALUE, value);
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    val mValues: List<Int> = arrayListOf(3, 4, 5, 6, 7)
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        if (arguments != null) {
+            mValue = mValues.indexOf(arguments.getInt(BUNDLE_VALUE, 3))
+        }
+
+        if (savedInstanceState != null) {
+            mValue = savedInstanceState.getInt(BUNDLE_VALUE, 0);
+        }
+
+        val dialog = MaterialDialog.Builder(activity)
+                .title("Choose Burst Amount")
+                .items(mValues)
+                .itemsCallbackSingleChoice(mValue, { materialDialog, view, i, charSequence ->
+                    mValue = i
+                    true
+
+                })
+                .alwaysCallSingleChoiceCallback()
+                .positiveText("ok")
+                .onPositive({ materialDialog, dialogAction -> mListener.amountChosen(mValues[mValue]) })
+                .show();
+
+        return dialog
+    }
+}
--- app/src/main/java/com/sthagios/stopmotion/create/CreateNewImage.kt
+++ app/src/main/java/com/sthagios/stopmotion/create/CreateNewImage.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.TextView
 import com.sthagios.stopmotion.R
 import com.sthagios.stopmotion.camera.CameraPreview
 
@@ -14,21 +16,103 @@ import com.sthagios.stopmotion.camera.CameraPreview
  * @author  stephan
  * @since   14.05.16
  */
-class CreateNewImage : AppCompatActivity() {
+class CreateNewImage : AppCompatActivity(), AbstractDialog.Callback {
+    override fun amountChosen(amount: Int) {
+        mBurstAmount = amount
+        setBurstTexts()
+    }
+
+    private val BUNDLE_BURST_TIME = "BUNDLE_BURST_TIME"
+
+    private val BUNDLE_BURST_AMOUNT = "BUNDLE_BURST_AMOUNT"
+
+    private var mBurstTime = 0
+
+    private var mBurstAmount = 3
+
+    override fun timeChosen(time: Int) {
+        mBurstTime = time
+        setBurstTexts()
+    }
+
+    private fun setBurstTexts() {
+        mAmountText.text = "$mBurstAmount"
+        mTimeText.text = resources.getStringArray(R.array.burst_times)[mBurstTime]
+    }
 
     private val TAG = "CreateNewImage"
+
+    private lateinit var mSwitchButton: ImageButton
+
+    private var mCameraId = 0
+
+    private lateinit var mTimeText: TextView
+
+    private lateinit var mAmountText: TextView
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState!!.putInt(BUNDLE_BURST_TIME, mBurstTime)
+        outState.putInt(BUNDLE_BURST_AMOUNT, mBurstAmount)
+        super.onSaveInstanceState(outState)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         setContentView(R.layout.activity_create_new_image)
 
-        safeCameraOpen(0)
+        if (savedInstanceState != null) {
+            mBurstTime = savedInstanceState.getInt(BUNDLE_BURST_TIME, 0)
+            mBurstAmount = savedInstanceState.getInt(BUNDLE_BURST_AMOUNT, 0)
+        }
+
+        safeCameraOpen(mCameraId)
         mPreview = CameraPreview(this, mCamera!!)
 
-        val preview = findViewById(R.id.camera_preview) as FrameLayout;
+        val preview = findViewById(R.id.camera_preview) as FrameLayout
         preview.addView(mPreview);
+
+        mTimeText = findViewById(R.id.time_textView) as TextView
+        mAmountText = findViewById(R.id.amount_text_view) as TextView
+
+
+        findViewById(R.id.container_time)!!.setOnClickListener({
+            onTimeClicked()
+        })
+        findViewById(R.id.container_amount)!!.setOnClickListener({
+            onAmountClicked()
+        })
+
+        mSwitchButton = findViewById(R.id.button_switch_camera) as ImageButton
+        mSwitchButton.setOnClickListener({
+            if (mCameraId == 0) {
+                mCameraId = 1
+                //TODO make this work
+//                mCamera!!.stopPreview();
+//                safeCameraOpen(mCameraId)
+//                mCamera!!.startPreview();
+                mSwitchButton.setImageDrawable(resources.getDrawable(R.drawable.ic_camera_rear_black_48dp))
+            } else {
+                mCameraId = 0
+                //TODO make this work
+//                mCamera!!.stopPreview();
+//                safeCameraOpen(mCameraId)
+//                mCamera!!.startPreview();
+                mSwitchButton.setImageDrawable(resources.getDrawable(R.drawable.ic_camera_front_black_48dp))
+            }
+        })
+
+        setBurstTexts()
+    }
+
+    private fun onTimeClicked() {
+        val dialog = BurstTimeDialog.newInstance(mBurstTime)
+        dialog.show(fragmentManager, "BurstTimingDialog")
+    }
+
+    private fun onAmountClicked() {
+        val dialog = BurstAmountDialog.newInstance(mBurstAmount)
+        dialog.show(fragmentManager, "BurstAmountDialog")
     }
 
     private var mCamera: Camera? = null
@@ -41,13 +125,13 @@ class CreateNewImage : AppCompatActivity() {
             mCamera = Camera.open(id)
             qOpened = (mCamera != null)
         } catch (e: Exception) {
-            Log.d(TAG, "Couldn' open camera" + e.message);
+            Log.d(TAG, "Couldn't open camera" + e.message);
         }
 
         return qOpened;
     }
 
-    private var mPreview: CameraPreview? = null
+    private lateinit var mPreview: CameraPreview
 
     private fun releaseCameraAndPreview() {
         if (mCamera != null) {
@@ -55,4 +139,5 @@ class CreateNewImage : AppCompatActivity() {
             mCamera = null
         }
     }
+
 }
