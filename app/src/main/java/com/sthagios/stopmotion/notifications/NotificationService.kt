13df2e1ddc81531db0d95a97f7b5b727f814b1c6
stephan <stephan.hagios@gmail.com>
2016-06-07 22:31:24 +0200

    Added messaging

---
+++ app/src/main/java/com/sthagios/stopmotion/notifications/NotificationService.kt
@@ -0,0 +1,21 @@
+package com.sthagios.stopmotion.notifications
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+/**
+ * Stopmotion
+ *
+ * @author  stephan
+ * @since   07.06.16
+ */
+class NotificationService : FirebaseMessagingService() {
+    private val TAG = "NotificationService"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        //TODO
+        Log.d(TAG, "From: " + remoteMessage!!.from);
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification.body);
+    }
+}
--- app/src/main/java/com/sthagios/stopmotion/show/ShowGifActivity.kt
+++ app/src/main/java/com/sthagios/stopmotion/show/ShowGifActivity.kt
@@ -14,6 +14,7 @@ import com.sthagios.stopmotion.utils.retrieveLongParameter
 import kotlinx.android.synthetic.main.activity_show_gif.*
 import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 
 class ShowGifActivity : AppCompatActivity() {
@@ -31,15 +32,18 @@ class ShowGifActivity : AppCompatActivity() {
         val uriThumb = Uri.parse(gif.thumbnailUriString)
 
         Observable.just(Glide.with(this).load(uriThumb).into(preview))
+//                .subscribeOn(Schedulers.computation())
                 .flatMap {
                     LogDebug("Loaded, loading gif")
                     Observable.just(Glide.with(this).load(uri).into(preview_gif))
+                            .subscribeOn(Schedulers.computation())
                 }
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({}, {
                     e ->
                     e.printStackTrace()
                 }, { loading_spinner.visibility = View.GONE })
+
 
         share_button.setOnClickListener({
             shareGif(gif.shareUriString)
