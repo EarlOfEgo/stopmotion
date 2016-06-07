a233b384a49e94b401f402ac0bdb4f269e68d4d8
stephan <stephan.hagios@gmail.com>
2016-06-07 22:09:05 +0200

    Added toolbar

--- app/src/main/java/com/sthagios/stopmotion/list/ImageListActivity.kt
+++ app/src/main/java/com/sthagios/stopmotion/list/ImageListActivity.kt
@@ -3,12 +3,16 @@ package com.sthagios.stopmotion.list
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import com.sthagios.stopmotion.R
 import com.sthagios.stopmotion.create.CreateNewImage
 import com.sthagios.stopmotion.image.database.Gif
 import com.sthagios.stopmotion.image.database.getRealmInstance
+import com.sthagios.stopmotion.settings.SettingsActivity
 import com.sthagios.stopmotion.utils.startActivity
 import kotlinx.android.synthetic.main.activity_image_list.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 /**
  * Stopmotion
@@ -21,6 +25,9 @@ class ImageListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_list)
+
+        setSupportActionBar(toolbar)
+
 
         recyclerViewImageList.setHasFixedSize(true)
         recyclerViewImageList.layoutManager = GridLayoutManager(this, 2)
@@ -41,4 +48,19 @@ class ImageListActivity : AppCompatActivity() {
         startActivity<CreateNewImage>()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater;
+        inflater.inflate(R.menu.main, menu);
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.settings -> {
+                startActivity<SettingsActivity>()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
