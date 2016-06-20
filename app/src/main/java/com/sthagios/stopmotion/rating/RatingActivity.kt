2f5b2892cd719d7348dcf184d5ae4b314490bd90
stephan <stephan.hagios@gmail.com>
2016-06-21 00:15:16 +0200

    Added rating tracking and stuff

--- app/src/main/java/com/sthagios/stopmotion/rating/RatingActivity.kt
+++ app/src/main/java/com/sthagios/stopmotion/rating/RatingActivity.kt
@@ -1,8 +1,13 @@
 package com.sthagios.stopmotion.rating
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.sthagios.stopmotion.R
+import com.sthagios.stopmotion.tracking.logRatingEvent
 
 /**
  * Stopmotion
@@ -12,27 +17,53 @@ import android.support.v7.app.AppCompatActivity
  */
 class RatingActivity : AppCompatActivity(), RatingDialog.Callback {
     override fun giveFeedback(feedback: Boolean) {
+        logRatingEvent("feedback", feedback)
         if (feedback) {
-            Snackbar.make(findViewById(android.R.id.content)!!, "oke", Snackbar.LENGTH_LONG).show()
-        } else
-            Snackbar.make(findViewById(android.R.id.content)!!, "meh", Snackbar.LENGTH_LONG).show()
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.type = "message/rfc822"
+            emailIntent.data = Uri.parse("mailto:" + "stopmotion@sthagios.com")
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT,
+                    getString(R.string.rating_feedback_email_title))
 
+            try {
+                startActivity(Intent.createChooser(emailIntent,
+                        getString(R.string.rating_email_choose_text)))
+            } catch (ex: android.content.ActivityNotFoundException) {
+                //TODO might add toast
+            }
+        } else {
+            Toast.makeText(this, "Thanks anyway", Toast.LENGTH_SHORT).show()
+        }
+        finish()
     }
 
     override fun rateTheApp(rate: Boolean) {
+        logRatingEvent("rate", rate)
         if (rate) {
-            Snackbar.make(findViewById(android.R.id.content)!!, "YEAH", Snackbar.LENGTH_LONG).show()
-        } else
-            Snackbar.make(findViewById(android.R.id.content)!!, "Nej", Snackbar.LENGTH_LONG).show()
-
+            val uri = Uri.parse("market://details?id=$packageName")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(
+                    Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            try {
+                startActivity(goToMarket);
+            } catch (e: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=$packageName")))
+            }
+        } else {
+            Toast.makeText(this, "Thanks anyway", Toast.LENGTH_SHORT).show()
+        }
+        finish()
     }
 
     override fun enjoyTheApp(enjoy: Boolean) {
+        logRatingEvent("enjoyed", enjoy)
         if (enjoy) {
-            mRatingRate.show(fragmentManager, "rate")
+            mRatingRate.show(fragmentManager, "RatingDialogRate")
         } else {
-            mRatingFeedback.show(fragmentManager, "Show")
-
+            mRatingFeedback.show(fragmentManager, "RatingDialogFeedback")
         }
     }
 
@@ -43,8 +74,12 @@ class RatingActivity : AppCompatActivity(), RatingDialog.Callback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mRatingFirst.show(fragmentManager, "RatingDialogFirst")
+    }
 
-        mRatingFirst.show(fragmentManager, "BLO")
-
+    override fun onBackPressed() {
+        super.onBackPressed()
+        logRatingEvent("back_pressed", true)
+        finish()
     }
 }
