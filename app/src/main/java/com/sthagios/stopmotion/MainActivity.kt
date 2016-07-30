45e67dfccf4f591b1b4a0309109607cba41ec0bc
stephan <stephan.hagios@gmail.com>
2016-07-30 21:04:51 +0200

    Added tiny easter egg

--- app/src/main/java/com/sthagios/stopmotion/MainActivity.kt
+++ app/src/main/java/com/sthagios/stopmotion/MainActivity.kt
@@ -139,7 +139,7 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                 }
-                return;
+                return
             }
         }
     }
