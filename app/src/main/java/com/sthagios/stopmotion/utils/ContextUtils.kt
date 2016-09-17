82f1995fe2355828b3ee3d278789ceafb3ef6fa6
stephan <stephan.hagios@gmail.com>
2016-09-17 11:29:09 +0200

    Moved logging from activity to context extensions

--- app/src/main/java/com/sthagios/stopmotion/utils/ContextUtils.kt
+++ app/src/main/java/com/sthagios/stopmotion/utils/ContextUtils.kt
@@ -3,6 +3,8 @@
 package com.sthagios.stopmotion.utils
 
 import android.content.Context
+import android.util.Log
+import com.sthagios.stopmotion.BuildConfig
 
 /**
  * Stopmotion
@@ -20,4 +22,19 @@ inline fun Context.getApproximateAppStarts() = getAppStartPreferences().getInt("
 inline fun Context.addAppStart() {
     val appstarts = getApproximateAppStarts() + 1
     getAppStartPreferences().edit().putInt("APP_START_AMOUNT", appstarts).apply()
+}
+
+inline fun Context.LogDebug(param: String) {
+    if (BuildConfig.DEBUG)
+        Log.d("${this.javaClass.simpleName}", param)
+}
+
+inline fun Context.LogVerbose(param: String) {
+    if (BuildConfig.DEBUG)
+        Log.v("${this.javaClass.simpleName}", param)
+}
+
+inline fun Context.LogError(param: String) {
+    if (BuildConfig.DEBUG)
+        Log.e("${this.javaClass.simpleName}", param)
 }
