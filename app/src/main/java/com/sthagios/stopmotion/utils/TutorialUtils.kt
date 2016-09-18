95893ba608b10c55b010de071edd97d2d87455a0
stephan <stephan.hagios@gmail.com>
2016-09-18 18:50:15 +0200

    don't show tutorial when in debug

--- app/src/main/java/com/sthagios/stopmotion/utils/TutorialUtils.kt
+++ app/src/main/java/com/sthagios/stopmotion/utils/TutorialUtils.kt
@@ -4,6 +4,7 @@ package com.sthagios.stopmotion.utils
 
 import android.content.Context
 import android.support.v4.content.ContextCompat
+import com.sthagios.stopmotion.BuildConfig
 import com.sthagios.stopmotion.R
 import com.wooplr.spotlight.SpotlightConfig
 
@@ -18,7 +19,7 @@ inline fun Context.getTutorialSharedPrefs() = getSharedPreferences("TUTORIAL", 0
 
 inline fun Context.shouldShowTutorial(): Boolean {
     val name = this.javaClass.simpleName
-    return getTutorialSharedPrefs().getBoolean(name, true)
+    return getTutorialSharedPrefs().getBoolean(name, true) && !BuildConfig.DEBUG
 }
 
 inline fun Context.showedTutorial() {
